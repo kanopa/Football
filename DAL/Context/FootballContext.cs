a8d07fc440a91e828ff40efa2bb05cad86c707ed
kanopa <konoplyanka1999@gmail.com>
2019-12-21 17:01:51 +0200

    add project

---
+++ DAL/Context/FootballContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Context
+{
+    class FootballContext : DbContext
+    {
+        public FootballContext(DbContextOptions<FootballContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
